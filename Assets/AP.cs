47f647388dff1f92e7666d2866de7679137c5635
mob-ippeitakagi <ippei.takagi@mobcast.jp>
2015-12-18 12:27:48 +0900

    アドベントカレンダー公開時のもの

---
+++ Assets/AP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UniRx;
+
+public class AP
+{
+	int maxAP = 10;
+	int recoveryInterval = 5;
+	public int MaxAP { get { return this.maxAP; } }
+	public ReactiveProperty<int> CurrentAP { get; private set; }
+	public ReactiveProperty<int> RecoveryTimer { get; private set; }
+	public ReadOnlyReactiveProperty<bool> IsMax { get; private set; }
+
+	public AP()
+	{
+		// 値の初期化
+		this.CurrentAP = new ReactiveProperty<int>(this.maxAP);
+		this.RecoveryTimer = new ReactiveProperty<int>(this.recoveryInterval);
+		this.IsMax = this.CurrentAP
+			.Select(x => x >= this.maxAP)
+			.DistinctUntilChanged()
+			.ToReadOnlyReactiveProperty();
+		// APが満タンじゃなくなったら回復処理を開始
+		this.IsMax
+			.Where(x => !x)
+			.Subscribe(_ => this.startRecovery());
+	}
+
+	void startRecovery()
+	{
+		Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1))
+			.Select(x => (int) (this.recoveryInterval - x))
+			.TakeWhile(x => x > 0)
+			.Subscribe(
+				x => this.RecoveryTimer.Value = x,
+				() =>
+				{
+					this.CurrentAP.Value++;
+					if (!this.IsMax.Value) this.startRecovery();
+				});
+	}
+}
---
+++ Assets/APGaugePresenter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class APGaugePresenter : MonoBehaviour
+{
+	int requiredAP = 3;
+	[SerializeField] Slider gauge;
+	[SerializeField] Text textValue;
+	[SerializeField] Text textTimer;
+	[SerializeField] Button button;
+
+	void Start()
+	{
+		var ap = new AP();
+
+		// ゲージのMAX値を設定
+		this.gauge.maxValue = ap.MaxAP;
+		// APの値をゲージとラベルに反映
+		ap.CurrentAP.Subscribe(x =>
+		{
+			this.gauge.value = x;
+			this.textValue.text = string.Format("{0} / {1}", x, ap.MaxAP);
+		});
+
+		// 回復時間をラベルに反映
+		ap.RecoveryTimer.Subscribe(x => this.textTimer.text = x.ToString());
+		// MAXの場合は回復時間を非表示
+		ap.IsMax.Subscribe(x => this.textTimer.gameObject.SetActive(!x));
+
+		// ボタンクリックでAP消費
+		this.button.OnClickAsObservable()
+			.Subscribe(_ =>
+			{
+				ap.CurrentAP.Value -= this.requiredAP;
+			});
+		// APが足りない場合はボタンが押せない
+		ap.CurrentAP.Select(x => x >= this.requiredAP).SubscribeToInteractable(this.button);
+	}
+}
